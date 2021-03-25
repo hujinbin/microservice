40ba478f625fa9d52d77a65720ede6f74293a4f0
hujinbin <437044048@qq.com>
2021-03-25 19:40:37 +0800

    build: 微服务和路由插件搭建

--- src/main.js
+++ src/main.js
@@ -2,4 +2,23 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 
+import { registerMicroApps, start } from 'qiankun';
+
+registerMicroApps([
+  {
+    name: 'react app', // app name registered
+    entry: '//localhost:7100',
+    container: '#yourContainer',
+    activeRule: '/yourActiveRule',
+  },
+  {
+    name: 'vue app',
+    entry: { scripts: ['//localhost:7100/main.js'] },
+    container: '#yourContainer2',
+    activeRule: '/yourActiveRule2',
+  },
+]);
+
+start();
+
 createApp(App).mount('#app')
