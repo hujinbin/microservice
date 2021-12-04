ce36d61c37a87b2de86ed3872d0af9914b09e8ba
hujinbin <437044048@qq.com>
2021-12-04 18:09:48 +0800

    build: 目录结构和主应用配置

--- src/main.js
+++ src/main.js
@@ -1,24 +1,61 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './index.css'
+import {
+  createApp
+} from 'vue'
+import App from './vue/App.vue'
+import './vue/index.css'
 
-import { registerMicroApps, start } from 'qiankun';
+import {
+  registerMicroApps,
+  setDefaultMountApp,
+  start
+} from 'qiankun';
 
-registerMicroApps([
-  {
-    name: 'react app', // app name registered
-    entry: '//localhost:7100',
-    container: '#yourContainer',
-    activeRule: '/yourActiveRule',
-  },
-  {
-    name: 'vue app',
-    entry: { scripts: ['//localhost:7100/main.js'] },
-    container: '#yourContainer2',
-    activeRule: '/yourActiveRule2',
-  },
-]);
-
-start();
+/**
+ * 路由监听
+ * @param {*} routerPrefix 前缀
+ */
+function genActiveRule(routerPrefix) {
+  return location => location.pathname.startsWith(routerPrefix);
+}
 
 createApp(App).mount('#app')
+
+registerMicroApps([{
+    name: 'vue app',
+    container: '#app',
+    activeRule: genActiveRule('/'),
+  },
+  {
+    name: 'react app', // app name registered
+    entry: '//localhost:3001/',
+    container: '#app',
+    activeRule: genActiveRule('/react'),
+  },
+  {
+    name: 'angular app', // app name registered
+    entry: '//localhost:3032/',
+    container: '#app',
+    activeRule: genActiveRule('/angular'),
+  },
+], {
+  beforeLoad: [
+    app => {
+      console.log("before load", app);
+    }
+  ], // 挂载前回调
+  beforeMount: [
+    app => {
+      console.log("before mount", app);
+    }
+  ], // 挂载后回调
+  afterUnmount: [
+    app => {
+      console.log("after unload", app);
+    }
+  ] // 卸载后回调
+});
+
+// 设置默认子应用
+setDefaultMountApp("/");
+
+start();
--- src/vue/components/HelloWorld.vue
+++ src/vue/components/HelloWorld.vue
@@ -2,6 +2,9 @@
   <h1>{{ msg }}</h1>
   <button @click="count++">count is: {{ count }}</button>
   <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <a href="/vue">vue子应用</a>
+  <a href="/react">react子应用</a>
+  <a href="/angular">angular子应用</a>
 </template>
 
 <script>
